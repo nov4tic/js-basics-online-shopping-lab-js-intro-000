c2cf5be287aecae693ed3c7a24e7cfefb5cdb6c0
Dev Frias <ddfrias14@gmail.com>
2021-03-07 07:15:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -46,7 +46,8 @@ function total() {
 function removeFromCart(item) {
   // write your code here
 for (let i = 0; i < cart.length; i++) {
-  if (item === cart.itemName('item')){
+  if (item === getCart()[i].itemName)
+  cart.splice(getCart()[i].itemName, 1)
 
   } else {
     return `That item is not in your cart`
