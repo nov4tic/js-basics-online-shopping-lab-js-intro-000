730714ecf9c52b19e0528d3d06572d4484c1b9eb
Dev Frias <ddfrias14@gmail.com>
2021-03-07 07:20:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ function removeFromCart(item) {
   // write your code here
 for (let i = 0; i < cart.length; i++) {
   if (item === getCart()[i].itemName){
-  cart.splice(getCart()[i].itemName, 1)
+  getCart().splice(getCart()[i].itemName, 1)
 } else { return 'That item is not in your cart.'}}}
 
 function placeOrder(cardNumber) {
