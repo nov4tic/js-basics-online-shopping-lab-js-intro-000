c682703bf24fb4e8110c1c65da4bec948435ced4
Dev Frias <ddfrias14@gmail.com>
2021-03-07 08:07:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -53,6 +53,6 @@ function removeFromCart(item) {
     return 'That item is not in your cart.'}
 
 function placeOrder(cardNumber) {
-  if (cardNumber) { 
+  if (cardNumber) {
   return `Sorry we don't have a credit card on file for you`
 }
