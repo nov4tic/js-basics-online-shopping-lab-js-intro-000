29ce201977f541089d243577a09f2c6c72bb5c06
Dev Frias <ddfrias14@gmail.com>
2021-03-07 08:14:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -54,8 +54,9 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if (cardNumber) {
+    endTotal = total()
     cart.splice(0, cart.length);
-    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}`;
+    return `Your total cost is $${endTotal}, which will be charged to the card ${cardNumber}`;
 } else {
     return `Sorry, we don't have a credit card on file for you.`
 }}
