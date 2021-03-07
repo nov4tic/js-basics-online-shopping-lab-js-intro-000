c8a59d496971c2bc36f2e52042ae474e6c14618b
Dev Frias <ddfrias14@gmail.com>
2021-03-07 06:53:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -45,7 +45,9 @@ function total() {
 
 function removeFromCart(item) {
   // write your code here
-
+for (let i = 0; i < cart.length; i++) {
+  if (item === cart.itemName('item'))
+}
 }
 
 function placeOrder(cardNumber) {
