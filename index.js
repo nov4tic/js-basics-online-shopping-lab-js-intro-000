e160b1810acbf34b71f2779702a28bff28efa6fc
Dev Frias <ddfrias14@gmail.com>
2021-03-07 08:08:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -54,5 +54,5 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if (cardNumber === undefined) {
-  return `Sorry we don't have a credit card on file for you`
+  return `Sorry, we don't have a credit card on file for you.`
 }}
