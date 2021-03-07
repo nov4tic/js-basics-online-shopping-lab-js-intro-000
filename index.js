9b4290c2b8be2c89e82a51ba655ad3427f499178
Dev Frias <ddfrias14@gmail.com>
2021-03-07 07:32:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -49,7 +49,8 @@ function removeFromCart(item) {
     if (item === cart[i].itemName) {
       cart.splice(i, 1)
       return cart
-    } else { return 'That item is not in your cart.'}}}
+    }
+    return 'That item is not in your cart.'}}
 
 function placeOrder(cardNumber) {
 
